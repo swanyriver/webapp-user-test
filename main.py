d689098f7e4f18b27c2fc26ee0b9edb0bdd5e1f5
Brandon Swanson <swanyriver@gmail.com>
2016-05-26 19:55:20 -0700

    working token, user_id field for get_by_auth_token must be the user key id and must be int

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import webapp2
+from google.appengine.ext.ndb import model
 from webapp2_extras import auth
 
 
@@ -66,10 +67,10 @@ class LogHandler(webapp2.RequestHandler):
             self.response.set_status(500)
             return
         else:
-            #print user
+            print user
             self.response.write("user logged in\n")
-            self.response.write("Hello %s\n"%(user.auth_ids[0]))
-            self.response.write("here is your token:\n%s\n"%user.create_auth_token(user.auth_ids[0]))
+            self.response.write("Hello %s  id:%s\n"%(user.auth_ids[0],user.key.id()))
+            self.response.write("here is your token:\n%s\n"%user.create_auth_token(user.key.id()))
             self.response.set_status(200)
             return
 
@@ -85,9 +86,23 @@ class TokenHandler(webapp2.RequestHandler):
             self.response.write('Missing Required Field\n')
             self.response.set_status(400, "Missing required field")
             return
-        user = auth.get_auth().store.user_model.get_by_auth_token(id, token)
+        user, timestamp = auth.get_auth().store.user_model.get_by_auth_token(int(id), token)
         print user
-        #self.response.write("Hello %s\n"%(user.auth_ids[0]))
+        self.response.write("Hello %s\n"%(user.auth_ids[0]))
+
+        # cls = auth.get_auth().store.user_model
+        #
+        # token_key = cls.token_model.get_key(id, 'auth', token)
+        # user_key = model.Key(cls, int(id))
+        # # Use get_multi() to save a RPC call.
+        # valid_token, user = model.get_multi([token_key, user_key])
+        # user = user_key.get()
+        # # if valid_token and user:
+        # #     timestamp = int(time.mktime(valid_token.created.timetuple()))
+        # #     return user, timestamp
+        # print "validToken:%r"%valid_token
+        # print "user", user, user_key
+
 
 app = webapp2.WSGIApplication([
     ('/register', RegHandler),
