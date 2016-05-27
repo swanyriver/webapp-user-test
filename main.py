136aecce4d25ce25ee8a0a8217f3ba0747e86782
Brandon Swanson <swanyriver@gmail.com>
2016-05-26 21:22:16 -0700

    returning json, token at register

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
 import webapp2
 from google.appengine.ext.ndb import model
 from webapp2_extras import auth
+import json
+
+
+def jsonMsg(response,msg):
+    response.write(json.dumps({"msg": msg}, indent=2) + '\n')
 
 
 class RegHandler(webapp2.RequestHandler):
     def post(self):
         print self.request.POST
 
-        id = self.request.POST.get('id')
+        name = self.request.POST.get('name')
         passw = self.request.POST.get('password')
 
-        if not id or not passw:
-            self.response.write('Missing Required Field\n')
+        if not name or not passw:
+            jsonMsg(self.response, 'Missing Required Field')
             self.response.set_status(400, "Missing required field")
             return
 
-        success, user = auth.get_auth().store.user_model.create_user(id, password_raw=passw)
+        success, user = auth.get_auth().store.user_model.create_user(name, password_raw=passw)
 
         if not success:
-            self.response.write('Unable to create that user:')
+            prefix='Unable to create that user:'
 
             if 'auth_id' in user:
-                self.response.write(" Username Already Taken\n")
+                jsonMsg(self.response, prefix + " Username Already Taken")
                 self.response.set_status(400)
             else:
-                self.response.write(" Server Error\n")
+                jsonMsg(self.response, prefix + " Server Error")
                 self.response.set_status(500)
             return
 
         else:
             print user
-            print type(user)
-            self.response.write('User Created\n')
-            self.response.set_status(202)
+            self.response.write(
+                json.dumps({"msg": "User Logged In",
+                            'userid': user.key.id(),
+                            'token': user.create_auth_token(user.key.id())},
+                           indent=2
+                           ))
+            self.response.set_status(200)
             return
 
 
@@ -40,41 +49,44 @@ class LogHandler(webapp2.RequestHandler):
     def post(self):
         print self.request.POST
 
-        id = self.request.POST.get('id')
+        name = self.request.POST.get('name')
         passw = self.request.POST.get('password')
 
-        if not id or not passw:
-            self.response.write('Hello world!')
+        if not name or not passw:
+            jsonMsg(self.response, 'Missing required field')
             self.response.set_status(400, "Missing required field")
             return
 
         print dir(auth.get_store())
 
         try:
-            #user = auth.get_auth().get_user_by_password(id, passw, save_session=False)
-            user = auth.get_auth().store.user_model.get_by_auth_password(id, passw)
+            #user = auth.get_auth().get_user_by_password(name, passw, save_session=False)
+            user = auth.get_auth().store.user_model.get_by_auth_password(name, passw)
         except auth.InvalidPasswordError:
-            self.response.write("Invalid Password\n")
+            jsonMsg(self.response, "Invalid Password")
             self.response.set_status(400)
             return
         except auth.InvalidAuthIdError:
-            self.response.write("User Does Not Exist\n")
+            jsonMsg(self.response, "User Does Not Exist")
             self.response.set_status(400)
             return
 
         if not user:
-            self.response.write("Login Failed\n")
+            jsonMsg(self.response, "Login Failed")
             self.response.set_status(500)
             return
         else:
             print user
-            self.response.write("user logged in\n")
-            self.response.write("Hello %s  id:%s\n"%(user.auth_ids[0],user.key.id()))
-            self.response.write("here is your token:\n%s\n"%user.create_auth_token(user.key.id()))
+            self.response.write(
+                json.dumps({"msg": "User Logged In",
+                            'userid': user.key.id(),
+                            'token': user.create_auth_token(user.key.id())},
+                           indent=2
+                           ))
             self.response.set_status(200)
             return
 
-
+#will be changed to function when brought into project
 class TokenHandler(webapp2.RequestHandler):
     def post(self):
         print self.request.POST
@@ -83,16 +95,16 @@ class TokenHandler(webapp2.RequestHandler):
         token = self.request.POST.get('token')
 
         if not id or not token:
-            self.response.write('Missing Required Field\n')
+            jsonMsg(self.response, 'Missing required field')
             self.response.set_status(400, "Missing required field")
             return
         if not id.isdigit():
-            self.response.write('Id must be numeric\n')
+            jsonMsg(self.response, 'ID must be numeric')
             self.response.set_status(400, "Missing required field")
             return
         user, timestamp = auth.get_auth().store.user_model.get_by_auth_token(int(id), token)
         print user
-        self.response.write("Hello %s\n"%(user.auth_ids[0]))
+        self.response.write("Hello %s"%(user.auth_ids[0]))
 
 
 app = webapp2.WSGIApplication([
